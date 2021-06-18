ddc05a60ec9d9ed0225ca5a5b23df464341efae1
moli-debugger <mkmohamedalimoli@gmail.com>
2021-06-18 22:32:08 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from IPython.core import magic
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+import spacy
+from spacy import displacy
+import re
+import pytesseract
+import  PyPDF2
+
+app = Flask(__name__)
+#pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+text=[]
+text_format=''
+DATE=[]
+Email=[]
+def extraction(name):
+    with open(name,'rb') as pdf_file:
+        read_pdf = PyPDF2.PdfFileReader(pdf_file)
+        number_of_pages = read_pdf.getNumPages()
+        for page_number in range(number_of_pages):   # use xrange in Py2
+            page = read_pdf.getPage(page_number)
+            page_content = page.extractText()
+            text.append(page_content)
+    return text
+def analysis(data):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(data)
+    p=displacy.render(doc, style="ent")
+    for i in doc.ents:
+        if(i.label_=='DATE'):
+            DATE.append(i)
+        if(i.label_=="ORG"):
+            Email.append(i)
+    return p
+
+
+
+@app.route('/')
+def initial():
+    return render_template('index.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        filename=f.filename
+        data=extraction(filename)
+        for text_data in data:
+           text_format=''+text_data
+        p=analysis(text_format)
+
+        return render_template("uploader.html")
+
+@app.route('/date')
+def date():
+    return render_template("uploader.html",DATE=DATE,msg=Email)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
